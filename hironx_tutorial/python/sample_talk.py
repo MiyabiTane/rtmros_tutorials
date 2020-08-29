2b66d9eeb6f06b4d1aeec0a38f8198ecfbb7994f
MiyabiTane <miyabitane@yahoo.co.jp>
2020-08-29 12:08:16 +0900

    ターミナルの出力と会話

--- hironx_tutorial/python/sample_talk.py
+++ hironx_tutorial/python/sample_talk.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import rospy
 import actionlib
 from speech_recognition_msgs.msg import SpeechRecognitionCandidates
@@ -11,7 +14,6 @@ class speak_class:
         self.sound = -3
         self.arg2 = 'ja'
         self.str_candidate = ''
-        self.low_confidence = ''
         rospy.init_node('hiro_talk')
         self.sub = rospy.Subscriber('/speech_to_text', SpeechRecognitionCandidates, self.talk_cb)
         self.actionlib_part = actionlib.SimpleActionClient('/robotsound_jp', SoundRequestAction)
@@ -19,35 +21,33 @@ class speak_class:
         #rospy.Timer(rospy.Duration(10), self.loopOnce)
 
     def talk_cb(self, msg):
+        #print("talk_cb")
         if msg.transcript:
-            for char, conf in zip(msg.transcript, msg.confidence):
+            for char in msg.transcript:
                 self.str_candidate += char
-                if conf < 0.4:
-                    self.low_confidence += '、'+char
             print("got words {}".format(self.str_candidate))
-            self.loopOnce
-    
-    def loopOnce(self):
-        speak_msg = SoundRequestGoal()
-        speak_msg.sound_request.volume = self.volume
-        speak_msg.sound_request.command = self.command
-        speak_msg.sound_request.sound = self.sound
+            
+            print("PC speaks")
+            speak_msg = SoundRequestGoal()
+            speak_msg.sound_request.volume = self.volume
+            speak_msg.sound_request.command = self.command
+            speak_msg.sound_request.sound = self.sound
 
-        if "はい" in self.str_candidate or "そうです" in self.str_candidate:
-            speak_msg.sound_request.arg = "よかったです"
-        elif "いいえ" in self.str_candidate or "違います" in self.str_candidate:
-            speak_msg.sound_request.arg = "ごめんなさい、次は頑張ります"
-        elif "さようなら" in self.str_candidate:
-            speak_msg.sound_request.arg = "さようなら"
-            self.sub.unregister()
-        else:
-            speak_msg.sound_request.arg = "さっき、しゃべったのは、" + self.str_candidate + "でしたね。" + self.low_confidence + "は自信がないのですが、合っていますか？"
+            if "はい" in self.str_candidate or "そうです" in self.str_candidate:
+                speak_msg.sound_request.arg = "よかったです"
+            elif "いいえ" in self.str_candidate or "違います" in self.str_candidate:
+                speak_msg.sound_request.arg = "ごめんなさい、次は頑張ります"
+            elif "さようなら" in self.str_candidate:
+                speak_msg.sound_request.arg = "さようなら"
+                self.sub.unregister()
+            else:
+                speak_msg.sound_request.arg = "さっき、しゃべったのは、" + self.str_candidate + "で合っていますか？"
+            print(speak_msg.sound_request.arg)
+
+            speak_msg.sound_request.arg2 = self.arg2
+            self.actionlib_part.send_goal(speak_msg)
+            self.str_candidate = ""
         
-        speak_msg.sound_request.arg2 = self.arg2
-        self.actionlib_part.send_goal(speak_msg)
-        self.str_candidate = ""
-        self.low_confidence = ""
-
 
 if __name__ == '__main__':
     try:
